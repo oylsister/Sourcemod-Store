e1396d3f62fb193d24f7b8f6b50f150f65e86f8f
Keith Warren <KeithAllenWarren@gmail.com>
2015-01-15 05:06:39 -0500

    Extra files to push.

---
+++ scripting/compile_all.bat
@@ -0,0 +1,44 @@
+spcomp.exe store-backend.sp
+move "store-backend.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-core.sp
+move "store-core.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-distributor.sp
+move "store-distributor.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-gifting.sp
+move "store-gifting.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-inventory.sp
+move "store-inventory.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-loadout.sp
+move "store-loadout.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-logging.sp
+move "store-logging.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-refund.sp
+move "store-refund.smx" "D:\Script\store\scripting\compiled"
+
+@echo ---
+
+spcomp.exe store-shop.sp
+move "store-shop.smx" "D:\Script\store\scripting\compiled"
+
+pause
